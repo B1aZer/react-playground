e13cc4acb6d33288cfb8e981a3d6e72921242f54
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2016-01-06 12:32:20 +0300

    Fixing logic

--- src/App.js
+++ src/App.js
@@ -27,13 +27,63 @@ class Counter extends Component {
   }
 }
 
-export class App extends Component {
+
+class Controls extends Component {
   render() {
     return (
+    <div>
+    <button onClick={ this.props.start }>Start</button>
+    <button onClick={ this.props.end }>End</button>
+    </div>
+    );
+  }
+}
+
+class Ball {
+  constructor(props) {
+    this.props = {};
+  }
+  update(props) {
+    this.props = props;
+    this.render();
+  }
+  render() {
+    var ctx = this.props.context;
+    ctx.fillStyle = "rgb(200,0,0)";
+    ctx.fillRect (10, 10, 55, 50);
+  }
+}
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    console.info(this.refs);
+    this.state = { context: null };
+    this.ball = new Ball();
+    this.startGame = this.startGame.bind(this);
+    this.endGame = this.endGame.bind(this);
+  }
+  componentDidMount() {
+    this.setState(Object.assign({}, this.state, {context: this.refs.canvas.getContext("2d") }));
+    requestAnimationFrame(() => {this.update()});
+  }
+  update() {
+    this.ball.update(this.state);
+  }
+  render() {
+    var started = this.state.started ? 'yes' : 'no';
+    return (
       <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
+        <span>{started}</span>
+        <canvas ref="canvas" width="500" height="500" />
+        <Controls start={this.startGame} end={this.endGame}/>
       </div>
     );
+  }
+  startGame() {
+    this.setState(Object.assign({}, this.state, {started: true}));
+  }
+  endGame() {
+    this.setState(Object.assign({}, this.state, {started: false}));
   }
 }
